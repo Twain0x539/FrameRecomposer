48017fdfe245ccee1bd27d4fa1685e226c7ef45e
Mihail Karaev <karaevmihail@gmail.com>
2024-05-12 16:49:53 +0500

    (feat!) add extract frames from video function

--- frame_recomposer.py
+++ frame_recomposer.py
@@ -1,3 +1,8 @@
+import cv2
+import numpy as np
+from PIL import Image
+import os
+from tqdm import tqdm
 
 
 class FrameRecomposer():
@@ -5,8 +10,43 @@ class FrameRecomposer():
     def __init__(self):
         pass
 
+    def extract(self, input_file: str, output_folder: str, force: bool = False, quiet: bool = False) -> None:
 
-    def extract(self, input_file: str, output_folder: str) -> bool:
-         pass
+        cap = cv2.VideoCapture(input_file)
+        totalFrames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+        if not cap.isOpened():
+            print("Error opening video stream or file")
+
+        video_name = os.path.splitext(input_file.split('/')[-1])[0]  # extract video name and remove extension
+
+        output_subfolder = os.path.join(output_folder, video_name)
+        if not os.path.exists(output_subfolder):
+            os.mkdir(output_subfolder)
+
+        if len(os.listdir(output_subfolder)) > 0:
+            if not force:
+                if not quiet:
+                    print("Output directory is not empty! Operation cancelled.")
+                    return
+            else:
+                if not quiet:
+                    print("Warning! Output directory is not empty")
+
+        print(f"Extracting video frames to {output_subfolder}\\")
+        ctr = 0
+        with tqdm(total=totalFrames) as pbar:
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                im = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+                frame_name = str(ctr).zfill(7)
+                save_path = os.path.join(output_subfolder, frame_name) + ".jpeg"
+                im.save(save_path)
+                pbar.update(1)
+                ctr += 1
+
+            cap.release()
+
     def merge(self, input_folder: str, output_file: str) -> bool:
         pass
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ if __name__ == '__main__':
     parser.add_argument("output", help="output folder path", default=None, type=str)
     parser.add_argument("--extract", "-e", help="extract frames mode", action="store_true", default=False)
     parser.add_argument("--merge", "-m", help="merge frames mode", action="store_true", default=False)
+    parser.add_argument("--quiet", "-q", help="disable logs", action="store_true", default=False)
+    parser.add_argument("--force", "-f", help="enable overwriting", action="store_true", default=False)
     args = parser.parse_args()
 
     if args.extract and args.merge:
@@ -22,4 +24,5 @@ if __name__ == '__main__':
     if args.merge:
         frame_recomposer.merge(args.input, args.output)
     else:
-        frame_recomposer.extract(args.input, args.output)
+        frame_recomposer.extract(input_file=args.input, output_folder=args.output,
+                                 force=args.force, quiet=args.quiet)
